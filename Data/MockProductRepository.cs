c850a1a58374d318e0cfe5cebba07a3656e77a97
amikia <amirkian81@gmail.com>
2025-01-23 14:57:16 +0330

    Added mock repository for Product

---
+++ Data/MockProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace Data
+{
+    public class MockProductRepository
+    {
+        List<Product> products = new List<Product>
+        {
+            new Product{ Id = 1, Name = "Product A", Category = Categories.Category1, Price = 100 },
+            new Product{ Id = 2, Name = "Product B", Category = Categories.Category1, Price = 150 },
+            new Product{ Id = 3, Name = "Product C", Category = Categories.Category2, Price = 120 },
+            new Product{ Id = 4, Name = "Product D", Category = Categories.Category3, Price = 200 },
+            new Product{ Id = 5, Name = "Product E", Category = Categories.Category1, Price = 80 }
+        };
+    }
+}
