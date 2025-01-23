ac6bd0ed42c7c37e613064fff521351a3b426530
amikia <amirkian81@gmail.com>
2025-01-23 14:26:47 +0330

    Entities Added

---
+++ Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class BaseEntity<TKey> 
+    {
+        public TKey Id { get; set; }
+    }
+
+    public class BaseEntity : BaseEntity<int>
+    {
+    }
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public Categories Category { get; set; }
+        public int Price { get; set; }
+
+    }
+
+    public enum Categories
+    {
+        Category1,
+        Category2,
+        Category3
+    }
+}
